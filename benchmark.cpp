717e1b0a8b99f610171bac87fae7e6bb6e499659
sehe <sgheeren@gmail.com>
2025-02-11 04:32:45 +0100

    Interesting tuning results
    Tuning both pools to 4 threads gives interesting results.
    
    ```sh
    + ./build/asyncio_benchmark server,blocking 32 20
    E main:265 Running 32 threads for 20 seconds
    E listener:189 Operation aborted.
    E main:276 Total duration: 20.0114s (shutdown took 0ms)
    E stats:199  --
    E stats:201 Total bytes read: 271191625728 12.6283 GiB/s
    E stats:205 Total bytes sent: 271191625728 12.6283 GiB/s
    
    + ./build/asyncio_benchmark server,blasio 32 20
    E main:265 Running 32 threads for 20 seconds
    E listener:189 Operation aborted.
    E main:276 Total duration: 20.0113s (shutdown took 0ms)
    E stats:199  --
    E stats:201 Total bytes read: 251038494720 11.6899 GiB/s
    E stats:205 Total bytes sent: 251038494720 11.6899 GiB/s
    
    + ./build/asyncio_benchmark server,asio 32 20
    E main:265 Running 0 threads for 20 seconds
    E listener:189 Operation aborted.
    E main:276 Total duration: 20.0105s (shutdown took 0ms)
    E stats:199  --
    E stats:201 Total bytes read: 324214980608 15.0974 GiB/s
    E stats:205 Total bytes sent: 324214980608 15.0974 GiB/s
    ```
    
    So, tabulated with CPU estimates:
    
     | Implementation   | Threads S + C  | CPU Usage    | GiB/s  |
     | -----------------|----------------|--------------| -------|
     | blocking C       | 1 + 32         | ~750%        | ~12.6  |
     | blocking asio    | 1 + 32         | ~950%        | ~11.7  |
     | asio             | 4 + 4          | ~800%        | ~15.09 |

--- benchmark.cpp
+++ benchmark.cpp
@@ -240,7 +240,7 @@ int main(int argc, char** argv) {
         size_t const njobs     = std::stoi(argv[2]);
 
         std::vector<std::jthread> threads;
-        asio::thread_pool         server_ctx(1);
+        asio::thread_pool         server_ctx(4);
         Executor                  ex = server_ctx.get_executor();
 
         if (selection.contains("server"))
@@ -249,7 +249,7 @@ int main(int argc, char** argv) {
         sleep_for(10ms); // allow server to start
         // co_spawn(ex, stats_thread, asio::detached);
 
-        asio::thread_pool client_ctx(1);
+        asio::thread_pool client_ctx(4);
 
         if (selection.contains("asio"))
             for (size_t i = 0; i < njobs; ++i)
